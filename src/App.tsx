27eb196f881d62d8e800f89c0bdf64a99fce8ec8
Mike Chamberlain <mikeachamberlain@gmail.com>
2018-08-06 01:41:43 +0700

    Icons and splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { createStackNavigator, NavigationScreenProps } from 'react-navigation';
-import { Font } from 'expo';
+import { Font, AppLoading } from 'expo';
 import { defaultStyles } from './styles';
 import { Home } from './Home/Home';
 
 export class App extends React.PureComponent<NavigationScreenProps> {
 
-  async componentDidMount() {
+  state = {
+    isReady: false,
+  };
+
+  async cacheResourcesAsync() {
     await Font.loadAsync({
       'johnston': require('./assets/fonts/Johnston100-Regular.otf'),
     });
-    this.props.navigation.replace('Home')
   }
 
-  render(): JSX.Element {
-    return null;
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.cacheResourcesAsync}
+          onFinish={() => this.props.navigation.replace('Home')}
+          onError={console.warn}
+        />
+      );
+    } else {
+      return null;
+    }
   }
 }
 
